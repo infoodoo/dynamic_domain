02113215bd8a919b8063705e72761a1329d4f97e
infoodoo <ayoubounasser5@gmail.com>
2025-05-26 12:09:42 +0100

    add modif

--- dynamic_domain_builder/__manifest__.py
+++ dynamic_domain_builder/__manifest__.py
@@ -10,10 +10,10 @@
     'author': "InfoLabWeb",
     'website': "https://infolabweb.odoo.com",
     'license': 'OPL-1',
-    'price': 18,
+    # 'price': 18,
     'currency': 'USD',
     'category': 'Uncategorized',
-    'version': '17.0',
+    # 'version': '17.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
